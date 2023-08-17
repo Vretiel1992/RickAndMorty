e18c6d97a4fffdc62a5437d82b24d690ec2f4351
Антон Денисюк <Vretiel@icloud.com>
2023-08-17 19:03:12 +0300

    Initial commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Антон Денисюк on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
